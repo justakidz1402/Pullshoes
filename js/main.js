4636b1e2a69f70bae743d367ebeaee1d9231afcf
DucNDA <justakidz1402@gmail.com>
2023-02-03 14:42:06 +0700

    Justakidz
    add login and search

--- js/main.js
+++ js/main.js
@@ -15,58 +15,57 @@ function getCategories(x) {
     console.log(x)
 }
 
-//Product Array
 let listProduct = [
-        {
-            productName: 'Best Shoes',
-            price: 60,
-            image: "images/shoes-img4.png",
-        },
-        {
-            productName: "Best Shoes",
-            price: "400",
-            image: "images/shoes-img5.png",
-        },
-        {
-            productName: "Best Shoes",
-            price: "50",
-            image: "images/shoes-img6.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "70",
-            image: "images/shoes-img7.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "100",
-            image: "images/shoes-img8.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "90",
-            image: "images/shoes-img9.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "70",
-            image: "images/shoes-img7.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "70",
-            image: "images/shoes-img7.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "70",
-            image: "images/shoes-img7.png",
-        },
-        {
-            productName: "Sports Shoes",
-            price: "70",
-            image: "images/shoes-img7.png",
-        },
+    {
+        productName: 'Best Shoes',
+        price: 60,
+        image: "images/shoes-img4.png",
+    },
+    {
+        productName: "Best Shoes",
+        price: "400",
+        image: "images/shoes-img5.png",
+    },
+    {
+        productName: "Best Shoes",
+        price: "50",
+        image: "images/shoes-img6.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "70",
+        image: "images/shoes-img7.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "100",
+        image: "images/shoes-img8.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "90",
+        image: "images/shoes-img9.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "70",
+        image: "images/shoes-img7.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "70",
+        image: "images/shoes-img7.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "70",
+        image: "images/shoes-img7.png",
+    },
+    {
+        productName: "Sports Shoes",
+        price: "70",
+        image: "images/shoes-img7.png",
+    },
 ];
 
 for (let i = 0; i < listProduct.length; i++) {
@@ -99,3 +98,59 @@ for (let i = 0; i < listProduct.length; i++) {
     }
     console.log(listProduct[i].name)
 }
+
+//Detail Page
+var singleProductPage = $('.single-product');
+singleProductPage.on('click', function (e) {
+    if (singleProductPage.hasClass('visible')) {
+        var clicked = $(e.target);
+        if (clicked.hasClass('close') || clicked.hasClass('overlay')) {
+            createQueryHash(filters);
+        }
+    }
+});
+
+function generateAllProductsHTML(data){
+    var list = $('.all-products .products-list');
+    var theTemplateScript = $("#products-template").html();
+    var theTemplate = Handlebars.compile (theTemplateScript);
+    list.append (theTemplate(data));
+    list.find('li').on('click', function (e) {
+        e.preventDefault();
+        var productIndex = $(this).data('index');
+        window.location.hash = 'product/' + productIndex;
+    })
+}
+
+function renderProductsPage(data){
+    var page = $('.all-products'),
+        allProducts = $('.all-products .products-list > li');
+    allProducts.addClass('hidden');
+    allProducts.each(function () {
+        var that = $(this);
+        data.forEach(function (item) {
+            if(that.data('index') == item.id){
+                that.removeClass('hidden');
+            }
+        });
+    });
+    page.addClass('visible');
+}
+
+function renderSingleProductPage(index, data) {
+    var page = $('.single-product'),
+        container = $('.preview-large');
+    if (data.length) {
+        data.forEach(function (item) {
+            if (item.id == index) {
+                container.find('h3').text(item.name);
+                container.find('img').attr('src', item.image.large);
+                container.find('p').text(item.description);
+            }
+        });
+    }
+    page.addClass('visible');
+}
+
+// Login
+$('#myModal').modal('show');
