30067fd9484d1047bd2ece47ee78d8b53040c05d
DucNDA <justakidz1402@gmail.com>
2023-02-04 15:10:34 +0700

    .
    .

--- js/main.js
+++ js/main.js
@@ -17,59 +17,45 @@ function getCategories(x) {
 
 let listProduct = [
     {
+        id: 1,
         productName: 'Best Shoes',
         price: 60,
         image: "images/shoes-img4.png",
     },
     {
+        id: 2,
         productName: "Best Shoes",
         price: "400",
         image: "images/shoes-img5.png",
     },
     {
+        id: 3,
         productName: "Best Shoes",
         price: "50",
         image: "images/shoes-img6.png",
     },
     {
+        id: 4,
         productName: "Sports Shoes",
         price: "70",
         image: "images/shoes-img7.png",
     },
     {
+        id: 5,
         productName: "Sports Shoes",
         price: "100",
         image: "images/shoes-img8.png",
     },
     {
+        id: 6,
         productName: "Sports Shoes",
         price: "90",
         image: "images/shoes-img9.png",
-    },
-    {
-        productName: "Sports Shoes",
-        price: "70",
-        image: "images/shoes-img7.png",
-    },
-    {
-        productName: "Sports Shoes",
-        price: "70",
-        image: "images/shoes-img7.png",
-    },
-    {
-        productName: "Sports Shoes",
-        price: "70",
-        image: "images/shoes-img7.png",
-    },
-    {
-        productName: "Sports Shoes",
-        price: "70",
-        image: "images/shoes-img7.png",
-    },
+    }
 ];
 
 for (let i = 0; i < listProduct.length; i++) {
-    var boxProduct = `<div class="col-sm-4">
+    var boxProduct = `<div class="col-sm-4" data-toggle="modal" data-target="#${listProduct[i].id}">
     <div class="best_shoes">
         <p class="best_text">${listProduct[i].productName}</p>
         <div class="shoes_icon"><img src='${listProduct[i].image}'></div>
@@ -88,6 +74,46 @@ for (let i = 0; i < listProduct.length; i++) {
             </div>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="${listProduct[i].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document" style="margin: 50px auto; min-width: 650px;">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">${listProduct[i].productName} Detail</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <section class="product">
+      <div class="product__photo">
+          <div class="photo-container">
+              <div class="photo-main">
+                  <img src='${listProduct[i].image}' alt="green apple slice" style="margin: 100px;">
+              </div>
+          </div>
+      </div>
+      <div class="product__info">
+          <div class="title">
+              <h1>${listProduct[i].productName}</h1>
+          </div>
+          <div class="price">
+              R$ <span>${listProduct[i].price}</span>
+          </div>
+          <div class="description">
+              <h3>BENEFITS</h3>
+              <ul>
+                  <li>Apples are nutricious</li>
+                  <li>Apples may be good for weight loss</li>
+                  <li>Apples may be good for bone health</li>
+                  <li>They're linked to a lowest risk of diabetes</li>
+              </ul>
+          </div>
+          <button class="buy--btn">ADD TO CART</button>
+      </div>
+  </section>
+      </div>
+  </div>
 </div>`
 
     if (listProduct[i].new === true) {
@@ -100,57 +126,7 @@ for (let i = 0; i < listProduct.length; i++) {
 }
 
 //Detail Page
-var singleProductPage = $('.single-product');
-singleProductPage.on('click', function (e) {
-    if (singleProductPage.hasClass('visible')) {
-        var clicked = $(e.target);
-        if (clicked.hasClass('close') || clicked.hasClass('overlay')) {
-            createQueryHash(filters);
-        }
-    }
-});
 
-function generateAllProductsHTML(data){
-    var list = $('.all-products .products-list');
-    var theTemplateScript = $("#products-template").html();
-    var theTemplate = Handlebars.compile (theTemplateScript);
-    list.append (theTemplate(data));
-    list.find('li').on('click', function (e) {
-        e.preventDefault();
-        var productIndex = $(this).data('index');
-        window.location.hash = 'product/' + productIndex;
-    })
-}
-
-function renderProductsPage(data){
-    var page = $('.all-products'),
-        allProducts = $('.all-products .products-list > li');
-    allProducts.addClass('hidden');
-    allProducts.each(function () {
-        var that = $(this);
-        data.forEach(function (item) {
-            if(that.data('index') == item.id){
-                that.removeClass('hidden');
-            }
-        });
-    });
-    page.addClass('visible');
-}
-
-function renderSingleProductPage(index, data) {
-    var page = $('.single-product'),
-        container = $('.preview-large');
-    if (data.length) {
-        data.forEach(function (item) {
-            if (item.id == index) {
-                container.find('h3').text(item.name);
-                container.find('img').attr('src', item.image.large);
-                container.find('p').text(item.description);
-            }
-        });
-    }
-    page.addClass('visible');
-}
 
 // Login
 $('#myModal').modal('show');
