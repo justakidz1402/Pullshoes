41550ebd397373e711648ac492527499c7817d09
DucNDA <justakidz1402@gmail.com>
2023-02-22 16:03:08 +0700

    sủa thành es6

--- js/main.js
+++ js/main.js
@@ -12,149 +12,104 @@ function getCategories(x) {
     for (var i = 0; i < document.getElementsByClassName(x).length; i++) {
         document.getElementsByClassName(x)[i].style.display = 'block';
     }
-    console.log(x)
 }
 
 //Products and Product detail
-let listProduct = [
-    {
-        id: 1,
-        productName: 'White Grey Shoes',
-        price: "60",
-        image: "images/shoes-img10.png",
-        category: 'all grey'
-    },
-    {
-        id: 2,
-        productName: "Purple Neon Shoes",
-        price: "400",
-        image: "images/shoes-img5.png",
-        category: 'all purple neon'
-
-    },
-    {
-        id: 3,
-        productName: "Green Red Shoes",
-        price: "50",
-        image: "images/shoes-img11.png",
-        category: 'all green'
-    },
-    {
-        id: 4,
-        productName: "Black Grey Shoes",
-        price: "70",
-        image: "images/shoes-img7.png",
-        category: 'all black grey'
-    },
-    {
-        id: 5,
-        productName: "Black Purple Shoes",
-        price: "100",
-        image: "images/running-shoes.png",
-        category: 'all black purple'
-    },
-    {
-        id: 6,
-        productName: "Blue Green Shoes",
-        price: "90",
-        image: "images/shoes-img9.png",
-        category: 'all blue green'
-    },
-    {
-        id: 7,
-        productName: "Neon Pink Shoes",
-        price: "100",
-        image: "images/shoes-img12.png",
-        category: 'all neon'
-    },
-    {
-        id: 8,
-        productName: "Green Yellow Shoes",
-        price: "80",
-        image: "images/shoes-img13.png",
-        category: 'all green'
-    },
-    {
-        id: 9,
-        productName: "Blue Green Shoes",
-        price: "110",
-        image: "images/shoes-img14.png",
-        category: 'all blue green'
-    }
-];
-
-for (let i = 0; i < listProduct.length; i++) {
-    var boxProduct = `<div class="col-sm-4 categoryShoes ${listProduct[i].category}" data-toggle="modal" data-target="#${listProduct[i].id}">
-    <div class="best_shoes">
-        <p class="best_text product-name">${listProduct[i].productName}</p>
-        <div class="shoes_icon"><img src='${listProduct[i].image}' style="width: 200px; height: 200px"></div>
-        <div class="star_text">
-            <div class="left_part">
-                <ul>
-                    <li><a href="#"><img src="images/star-icon.png"></a></li>
-                    <li><a href="#"><img src="images/star-icon.png"></a></li>
-                    <li><a href="#"><img src="images/star-icon.png"></a></li>
-                    <li><a href="#"><img src="images/star-icon.png"></a></li>
-                    <li><a href="#"><img src="images/star-icon.png"></a></li>
-                </ul>
-            </div>
-            <div class="right_part">
-                <div class="shoes_price"><span style="color: #ff4e5b;">${listProduct[i].price}</span>$</div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade detailModal" id="${listProduct[i].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document" style="margin: 50px auto; min-width: 650px;">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">${listProduct[i].productName} Detail</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true" class="close-detail">&times;</span>
-        </button>
-      </div>
-      <section class="product">
-      <div class="product__photo">
-          <div class="photo-container">
-              <div class="photo-main img-product">
-                  <img class="img-prd" src='${listProduct[i].image}' alt="green apple slice" style="margin: 100px; width: 200px; height: 200px">
-              </div>
-          </div>
-      </div>
-      <div class="product__info">
-          <div class="title">
-              <h1 class="content-product-h1">${listProduct[i].productName}</h1>
-          </div>
-          <div class="price">
-               <span class="span">${listProduct[i].price}$</span>
-          </div>
-          <div class="description">
-              <h3>DESCRIPTION</h3>
-              <ul>
-                  <li>Apples are nutricious</li>
-                  <li>Apples may be good for weight loss</li>
-                  <li>Apples may be good for bone health</li>
-                  <li>They're linked to a lowest risk of diabetes</li>
-              </ul>
-          </div>
-          <button class="buy--btn btn-cart">ADD TO CART</button>
-      </div>
-  </section>
-      </div>
-  </div>
-</div>`
-
-    if (listProduct[i].new === true) {
-        document.getElementById("renderProduct").innerHTML += boxProductNew
-    }
-    else {
-        document.getElementById("renderProduct").innerHTML += boxProduct
+class Shoe {
+    constructor(id, productName, price, image, category) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.image = image;
+        this.category = category;
     }
 }
 
+let listProduct = [
+    new Shoe(1, 'White Grey Shoes', "60", "images/shoes-img10.png", 'all grey'),
+    new Shoe(2, "Purple Neon Shoes", "400", "images/shoes-img5.png", 'all purple neon'),
+    new Shoe(3, "Green Red Shoes", "50", "images/shoes-img11.png", 'all green'),
+    new Shoe(4, "Black Grey Shoes", "70", "images/shoes-img7.png", 'all black grey'),
+    new Shoe(5, "Black Purple Shoes", "100", "images/running-shoes.png", 'all black purple'),
+    new Shoe(6, "Blue Green Shoes", "90", "images/shoes-img9.png", 'all blue green'),
+    new Shoe(7, "Neon Pink Shoes", "100", "images/shoes-img12.png", 'all neon'),
+    new Shoe(8, "Green Yellow Shoes", "80", "images/shoes-img13.png", 'all green'),
+    new Shoe(9, "Blue Green Shoes", "110", "images/shoes-img14.png", 'all blue green')
+];
+
+let render = (listProduct = []) => {
+    let boxProduct = ``;
+    listProduct.map(({ id, productName, price, image, category }) => {
+        boxProduct += `
+        <div class="col-sm-4 categoryShoes ${category}" data-toggle="modal" data-target="#${id}">
+        <div class="best_shoes">
+            <p class="best_text product-name">${productName}</p>
+            <div class="shoes_icon"><img src='${image}' style="width: 200px; height: 200px"></div>
+            <div class="star_text">
+                <div class="left_part">
+                    <ul>
+                        <li><a href="#"><img src="images/star-icon.png"></a></li>
+                        <li><a href="#"><img src="images/star-icon.png"></a></li>
+                        <li><a href="#"><img src="images/star-icon.png"></a></li>
+                        <li><a href="#"><img src="images/star-icon.png"></a></li>
+                        <li><a href="#"><img src="images/star-icon.png"></a></li>
+                    </ul>
+                </div>
+                <div class="right_part">
+                    <div class="shoes_price"><span style="color: #ff4e5b;">${price}</span>$</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="modal fade detailModal" id="${id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+      <div class="modal-dialog" role="document" style="margin: 50px auto; min-width: 650px;">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">${productName} Detail</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true" class="close-detail">&times;</span>
+            </button>
+          </div>
+          <section class="product">
+          <div class="product__photo">
+              <div class="photo-container">
+                  <div class="photo-main img-product">
+                      <img class="img-prd" src='${image}' alt="green apple slice" style="margin: 100px; width: 200px; height: 200px">
+                  </div>
+              </div>
+          </div>
+          <div class="product__info">
+              <div class="title">
+                  <h1 class="content-product-h1">${productName}</h1>
+              </div>
+              <div class="price">
+                   <span class="span">${price}$</span>
+              </div>
+              <div class="description">
+                  <h3>DESCRIPTION</h3>
+                  <ul>
+                      <li>Apples are nutricious</li>
+                      <li>Apples may be good for weight loss</li>
+                      <li>Apples may be good for bone health</li>
+                      <li>They're linked to a lowest risk of diabetes</li>
+                  </ul>
+              </div>
+              <button class="buy--btn btn-cart" onclick="AddToCart('${productName}','${price}','${image}')">ADD TO CART</button>
+          </div>
+      </section>
+          </div>
+      </div>
+    </div>   
+        `
+    })
+    document.getElementById('renderProduct').innerHTML = boxProduct;
+}
+render(listProduct);
+
+
 //Filter
-function filterProduct(x) {
+let Filter = (x) => {
     document.getElementById('all').classList.remove('activeShoes')
     document.getElementById('grey').classList.remove('activeShoes')
     document.getElementById('purple').classList.remove('activeShoes')
@@ -163,126 +118,105 @@ function filterProduct(x) {
     document.getElementById('green').classList.remove('activeShoes')
     document.getElementById('black').classList.remove('activeShoes')
     document.getElementById(x).classList.add("activeShoes")
+
     for (var i = 0; i < 9; i++) {
         document.getElementsByClassName('categoryShoes')[i].style.display = 'none';
     }
     for (var i = 0; i < document.getElementsByClassName(x).length; i++) {
         document.getElementsByClassName(x)[i].style.display = 'block';
     }
-    console.log(x)
 }
 
 //Search
-document.getElementById("search").addEventListener("click", () => {
-    //initializations
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".categoryShoes");
-    //loop through all elements
-    elements.forEach((element, index) => {
-        //check if text includes the search value
-        if (((element.innerText).toUpperCase()).includes(searchInput.toUpperCase())) {
-            //display matching card
-            cards[index].classList.remove("hide");
-        } else {
-            //hide others
-            document.getElementsByClassName('categoryShoes')[index].style.display = 'none';
-        }
-        console.log(element);
-    });
-});
+let Search = () => {
+    let name = document.getElementById("search-input").value;
+    let searchList = listProduct.filter(e => (e.productName).toLowerCase().includes(name.toLowerCase()));
+    render(searchList);
+}
 
 // Login
 // $('#myModal').modal('show');
 
 //Giỏ Hàng
+class Cart {
+    constructor(id, title, price, image, quantity) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.image = image;
+        this.quantity = quantity;
+    }
+}
+let listCart = [];
+var productIdCounter = 0;
+let showCart = (listCart = []) => {
+    let row = ``;
+    listCart.map(({ id, title, price, image, quantity }) => {
+        row += `<div class="d-flex cart-row">
+        <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${image}" width="100" height="100">
+        <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input id='quantity-${id}' class="cart-quantity-input" type="number" value="${quantity}">
+        <button class="btn btn-danger" type="button" onclick="Delete(${id})"><i class="fa fa-trash"></i></button>
+    </div>  </div> 
+        `
+    })
+    document.getElementById('cart-items').innerHTML = row;
+}
+showCart(listCart);
+
+let AddToCart = (title, price, image) => {
+    let id = ++productIdCounter;
+    let product = new Cart(id, title, price, image, 1);
+
+    let findRs = (title) => listCart.find(e => e.title == title);
+    if (findRs(product.title) == undefined) {
+        listCart.push(product);
+        showCart(listCart);
+        UpdateQuantity();
+        Total();
+        $('#cartModal').modal('show');
+        $('.detailModal').modal('hide');
+    } else {
+        alert('This product is already in your cart!');
+        $('#cartModal').modal('show');
+        $('.detailModal').modal('hide');
+    }
+    console.log(listCart);
+}
+
 var order = document.getElementsByClassName("order")[0];
 order.onclick = function () {
     alert('Mua hàng thành công');
 }
 
 // xóa cart
-var remove_cart = document.getElementsByClassName("btn-danger");
-debugger;
-for (var i = 0; i < remove_cart.length; i++) {
-    var button = remove_cart[i]
-    button.addEventListener("click", function () {
-        var button_remove = event.target
-        button_remove.parentElement.parentElement.remove()
-    })
+let Delete = (x) => {
+    listCart = listCart.filter(e => e.id !== x);
+    Total();
+    showCart(listCart);
 }
 
 // update cart 
-function updatecart() {
-    var cart_item = document.getElementsByClassName("cart-items")[0];
-    var cart_rows = cart_item.getElementsByClassName("cart-row");
-    var total = 0;
-    for (var i = 0; i < cart_rows.length; i++) {
-        var cart_row = cart_rows[i]
-        var price_item = cart_row.getElementsByClassName("cart-price")[0]
-        var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
-        var price = parseFloat(price_item.innerText)// chuyển một chuổi string sang number để tính tổng tiền.
-        var quantity = quantity_item.value // lấy giá trị trong thẻ input
-        total = total + (price * quantity)
-    }
-    document.getElementsByClassName("cart-total-price")[0].innerText = total;
-    // Thay đổi text = total trong .cart-total-price. Chỉ có một .cart-total-price nên mình sử dụng [0].
+let Total = () => {
+    let total = listCart.reduce((sum, shoe) => parseInt(sum) + parseInt(shoe.price) * parseInt(shoe.quantity), 0);
+    document.getElementById('total-price').innerText = total;
 }
 
-// Thêm vào giỏ
-var add_cart = document.getElementsByClassName("btn-cart");
-for (var i = 0; i < add_cart.length; i++) {
-    var add = add_cart[i];
-    add.addEventListener("click", function (event) {
-        var button = event.target;
-        var product = button.parentElement.parentElement;
-        var img = product.parentElement.getElementsByClassName("img-prd")[0].src
-        var title = product.getElementsByClassName("content-product-h1")[0].innerText
-        var price = product.getElementsByClassName("price")[0].innerText
-        addItemToCart(title, price, img)
-        // Khi thêm sản phẩm vào giỏ hàng thì sẽ hiển thị modal
-        $('#cartModal').modal('show');
-        $('.detailModal').modal('hide');
-        updatecart();
-    })
-
+let UpdateQuantity = () => {
+    listCart.forEach((product) => {
+        const input = document.getElementById(`quantity-${product.id}`);
+        input.addEventListener('input', (event) => {
+            const newQuantity = parseInt(event.target.value);
+            product.quantity = newQuantity;
+            Total();
+        });
+    });
 }
 
-function addItemToCart(title, price, img) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cart_title = cartItems.getElementsByClassName('cart-item-title')
-    // Nếu title của sản phẩm bằng với title mà bạn thêm vao giỏ hàng thì sẽ thông cho user.
-    for (var i = 0; i < cart_title.length; i++) {
-        if (cart_title[i].innerText == title) {
-            alert('Sản Phẩm Đã Có Trong Giỏ Hàng')
-            return;
-        }
-    }
 
-    var cartRowContents = `
-  <div class="cart-item cart-column">
-      <img class="cart-item-image" src="${img}" width="100" height="100">
-      <span class="cart-item-title">${title}</span>
-  </div>
-  <span class="cart-price cart-column">${price}</span>
-  <div class="cart-quantity cart-column">
-      <input class="cart-quantity-input" type="number" value="1">
-      <button class="btn btn-danger" type="button"><i class="fa fa-trash"></i></button>
-  </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', function () {
-        var button_remove = event.target;
-        button_remove.parentElement.parentElement.remove();
-        updatecart();
-    })
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', function (event) {
-        var input = event.target;
-        if (isNaN(input.value) || input.value <= 0) {
-            input.value = 1;
-        }
-        updatecart();
-    })
-}
+
+
